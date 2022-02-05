946f7d19d790dd13c632b4060f912c1dccf71e08
rootVIII <colleyloyejames@gmail.com>
2022-02-05 03:13:31 -0500

    show status/selection message

--- clickable-table.js
+++ clickable-table.js
@@ -4,11 +4,27 @@ function clearStatus() {
     document.getElementById('statusMsg').innerHTML = '&thinsp;';
 }
 
+function deselectCells(btnId = null) {
+    return new Promise((resolve) => {
+        const tdBtns = document.getElementsByClassName('table-button');
+        for (let tdBtn of tdBtns) {
+            if (btnId !== tdBtn.id && tdBtn.style.backgroundColor.length) {
+                const td = document.getElementById(`t${tdBtn.id.slice(1)}`);
+                tdBtn.style.color = '#212529';
+                tdBtn.style.backgroundColor = 'transparent';
+                td.style.setProperty('color', '#F8F9FA', 'important');
+                td.style.backgroundColor = '#F8F9FA';
+            }
+        }
+        resolve(null);
+    });
+}
+
 function selectCell(btnId) {
     return new Promise((resolve) => {
         const td = document.getElementById(`t${btnId.slice(1)}`);
         const btn = document.getElementById(btnId);
-        btn.style.setProperty('color', '#f8f9fa', 'important');
+        btn.style.setProperty('color', '#F8F9FA', 'important');
         btn.style.backgroundColor = '#188CFF';
         td.style.backgroundColor = '#188CFF';
         document.getElementById('statusMsg').innerHTML = btn.innerText.trim();
@@ -16,19 +32,28 @@ function selectCell(btnId) {
     });
 }
 
+function setSelection() {
+    const statusMsg = document.getElementById('statusMsg');
+    for (let tdBtn of document.getElementsByClassName('table-button')) {
+        console.log(tdBtn.style.backgroundColor.trim());
+        if (tdBtn.style.backgroundColor === 'rgb(24, 140, 255)') {
+            console.log(tdBtn.id);
+            statusMsg.innerHTML = `Selected ${tdBtn.innerText.trim()}`;
+        }
+    }
+}
+
 function main() {
     document.addEventListener('click', (event) => {
         if (event.target.className.includes('table-button')) {
-            selectCell(event.target.id).catch((err) => {
-                console.log(err);
-            });
+            selectCell(event.target.id).catch((err) => { console.log(err); });
         } else if (event.target.id === 'resetBtn') {
-            window.location.reload();
+            deselectCells().catch((err) => { console.log(err); });
         } else if (event.target.id === 'submitBtn') {
-            document.getElementById('statusMsg').innerHTML = 'Selected ...';
+            setSelection();
             setTimeout(() => {
                 clearStatus();
-            }, 5000);
+            }, 4000);
         }
     });
 }
